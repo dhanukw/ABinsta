9383971679ee87bc95d3679b6e9b821867e2a6f5
Grant Wiesnoski <grantalexanderw@gmail.com>
2014-03-06 03:28:07 -0800

    Linked profile clicks to friendslike

--- public/js/initializeClass.js
+++ public/js/initializeClass.js
@@ -1,11 +1,22 @@
+//var obj = require ( './Fsdafsdfrienddata.json' );
+var array = new Array();
+array[0] = "/melissaprofile";
+array[1] = "/brianprofile";
+array[2] = "/grantprofile";
+array[3] = "/snoopprofile";
+array[4] = "/imaginedprofile";
+
 $(document).ready(function(event) {
 	students = $('table.classmates tbody tr');
 	students.filter = filterStudents;
-	//console.log(students);
+//	console.log(students[0].align);
+//		console.log(students[0]);
+//		console.log(array[0]);
+//		console.log($(this).attr('id'));
 
 	$('.classmates tr').click(function() {
-		console.log($(this).attr('id'));
-		goToProfile($(this).attr('id'));
+		console.log($(this).attr('align'));
+		goToProfile($(this).attr('align'));
 	});
 
 	$('#filter').keyup(function(e) {
@@ -14,7 +25,7 @@ $(document).ready(function(event) {
 });
 
 function goToProfile(studentId) {
-	window.location.href = "profile?id=" + studentId;
+	window.location.href = studentId;
 }
 
 // Hides rows that don't contain a match for the name
